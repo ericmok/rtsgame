6560c02764707c56bd7ad6f27d70602ac356e7d0
Eric Mok <ericmok2@gmail.com>
2017-12-11 18:33:48 -0500

    Spaces converted to tabs.

--- core/src/nyc/mok/game/utils/ScaledSprite.java
+++ core/src/nyc/mok/game/utils/ScaledSprite.java
@@ -6,37 +6,33 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import nyc.mok.game.Constants;
 
-/**
- * Created by taco on 12/10/17.
- */
-
 public class ScaledSprite extends Sprite {
 
-    public ScaledSprite() {
-        super();
-    }
+	public ScaledSprite() {
+		super();
+	}
 
-    public ScaledSprite(Texture texture) {
-        super(texture);
-    }
+	public ScaledSprite(Texture texture) {
+		super(texture);
+	}
 
-    public void scaledDraw(SpriteBatch spriteBatch, float x, float y, float angleInDegrees) {
-        setSize(2 * getTexture().getWidth() * Constants.PIXEL_TO_METERS, 2 * getTexture().getHeight() * Constants.PIXEL_TO_METERS);
-        setOrigin(getWidth() / 2, getHeight() / 2);
+	public void scaledDraw(SpriteBatch spriteBatch, float x, float y, float angleInDegrees) {
+		setSize(2 * getTexture().getWidth() * Constants.PIXEL_TO_METERS, 2 * getTexture().getHeight() * Constants.PIXEL_TO_METERS);
+		setOrigin(getWidth() / 2, getHeight() / 2);
 
-        setPosition(x - getWidth() / 2, y - getHeight() / 2);
-        setRotation(angleInDegrees);
+		setPosition(x - getWidth() / 2, y - getHeight() / 2);
+		setRotation(angleInDegrees);
 
-        super.draw(spriteBatch);
-    }
+		super.draw(spriteBatch);
+	}
 
-    public void scaledDraw(SpriteBatch spriteBatch, float x, float y, float dx, float dh, float angleInDegrees) {
-        setSize(2 * dx * getTexture().getWidth() * Constants.PIXEL_TO_METERS, 2 * dh * getTexture().getHeight() * Constants.PIXEL_TO_METERS);
-        setOrigin(getWidth() / 2, getHeight() / 2);
+	public void scaledDraw(SpriteBatch spriteBatch, float x, float y, float dx, float dh, float angleInDegrees) {
+		setSize(2 * dx * getTexture().getWidth() * Constants.PIXEL_TO_METERS, 2 * dh * getTexture().getHeight() * Constants.PIXEL_TO_METERS);
+		setOrigin(getWidth() / 2, getHeight() / 2);
 
-        setPosition(x - getWidth() / 2, y - getHeight() / 2);
-        setRotation(angleInDegrees);
+		setPosition(x - getWidth() / 2, y - getHeight() / 2);
+		setRotation(angleInDegrees);
 
-        super.draw(spriteBatch);
-    }
+		super.draw(spriteBatch);
+	}
 }
