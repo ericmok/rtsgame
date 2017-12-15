c0f5e267983d9023e9c59907efa0829599709685
Eric Mok <ericmok2@gmail.com>
2017-12-14 22:34:34 -0500

    Converted spaces to tabs.

--- core/src/nyc/mok/game/units/Marine.java
+++ core/src/nyc/mok/game/units/Marine.java
@@ -11,66 +11,66 @@ import nyc.mok.game.components.EntityType;
 
 public class Marine {
 
-    public static final int HP = 10;
-    public static final float SWING_TIME = 0.5f;
-    public static final float COOLDOWN_TIME = 0.5f;
+	public static final int HP = 10;
+	public static final float SWING_TIME = 0.5f;
+	public static final float COOLDOWN_TIME = 0.5f;
 
-    /**
-     * Creates a marine.
-     *
-     * TODO: Needs recycling.
-     */
-    public static Entity create(World ecs, PlayerManager playerManager, String player, float x, float y) {
-        //Entity e = Common.create(ecs, x, y, Common.createDynamicBodyDef(x, y), Common.createCircleFixtureDef(1));
-        Entity e = Common.create(ecs, playerManager, player, x, y);
+	/**
+	 * Creates a marine.
+	 *
+	 * TODO: Needs recycling.
+	 */
+	public static Entity create(World ecs, PlayerManager playerManager, String player, float x, float y) {
+		//Entity e = Common.create(ecs, x, y, Common.createDynamicBodyDef(x, y), Common.createCircleFixtureDef(1));
+		Entity e = Common.create(ecs, playerManager, player, x, y);
 
-        ecs.getMapper(EntityType.class).get(e).type = EntityType.Type.MARINE;
+		ecs.getMapper(EntityType.class).get(e).type = EntityType.Type.MARINE;
 
-        BattleAttackableComponent battleAttackable = ecs.getMapper(BattleAttackableComponent.class).get(e);
-        battleAttackable.hp = HP;
-        battleAttackable.armorType = BattleAttackableComponent.ArmorType.ROCK;
+		BattleAttackableComponent battleAttackable = ecs.getMapper(BattleAttackableComponent.class).get(e);
+		battleAttackable.hp = HP;
+		battleAttackable.armorType = BattleAttackableComponent.ArmorType.ROCK;
 
-        BattleBehaviorComponent battleBehaviorComponent = ecs.getMapper(BattleBehaviorComponent.class).get(e);
-        battleBehaviorComponent.swingTime = SWING_TIME;
-        battleBehaviorComponent.cooldownTime = COOLDOWN_TIME;
-        battleBehaviorComponent.attackType = BattleBehaviorComponent.AttackType.ROCK;
+		BattleBehaviorComponent battleBehaviorComponent = ecs.getMapper(BattleBehaviorComponent.class).get(e);
+		battleBehaviorComponent.swingTime = SWING_TIME;
+		battleBehaviorComponent.cooldownTime = COOLDOWN_TIME;
+		battleBehaviorComponent.attackType = BattleBehaviorComponent.AttackType.ROCK;
 
-        return e;
-    }
+		return e;
+	}
 
-    public static Entity createTriangle(World ecs, PlayerManager playerManager, String player, float x, float y) {
-        Entity e = Common.create(ecs, playerManager, player, x, y);
+	public static Entity createTriangle(World ecs, PlayerManager playerManager, String player, float x, float y) {
+		Entity e = Common.create(ecs, playerManager, player, x, y);
 
-        ecs.getMapper(EntityType.class).get(e).type = EntityType.Type.TRIANGLE;
+		ecs.getMapper(EntityType.class).get(e).type = EntityType.Type.TRIANGLE;
 
-        BattleBehaviorComponent battleBehaviorComponent = ecs.getMapper(BattleBehaviorComponent.class).get(e);
-        battleBehaviorComponent.swingTime = SWING_TIME;
-        battleBehaviorComponent.cooldownTime = COOLDOWN_TIME;
-        battleBehaviorComponent.attackType = BattleBehaviorComponent.AttackType.PAPER;
+		BattleBehaviorComponent battleBehaviorComponent = ecs.getMapper(BattleBehaviorComponent.class).get(e);
+		battleBehaviorComponent.swingTime = SWING_TIME;
+		battleBehaviorComponent.cooldownTime = COOLDOWN_TIME;
+		battleBehaviorComponent.attackType = BattleBehaviorComponent.AttackType.PAPER;
 
-        BattleAttackableComponent battleAttackable = ecs.getMapper(BattleAttackableComponent.class).get(e);
-        battleAttackable.hp = HP;
-        battleAttackable.armorType = BattleAttackableComponent.ArmorType.PAPER;
+		BattleAttackableComponent battleAttackable = ecs.getMapper(BattleAttackableComponent.class).get(e);
+		battleAttackable.hp = HP;
+		battleAttackable.armorType = BattleAttackableComponent.ArmorType.PAPER;
 
-        return e;
-    }
+		return e;
+	}
 
-    public static Entity createSquare(World ecs, PlayerManager playerManager, String player, float x, float y) {
-        Entity e = Common.create(ecs, playerManager, player, x, y);
+	public static Entity createSquare(World ecs, PlayerManager playerManager, String player, float x, float y) {
+		Entity e = Common.create(ecs, playerManager, player, x, y);
 
-        ecs.getMapper(EntityType.class).get(e).type = EntityType.Type.SQUARE;
+		ecs.getMapper(EntityType.class).get(e).type = EntityType.Type.SQUARE;
 
-        BattleBehaviorComponent battleBehaviorComponent = ecs.getMapper(BattleBehaviorComponent.class).get(e);
-        battleBehaviorComponent.swingTime = SWING_TIME;
-        battleBehaviorComponent.cooldownTime = COOLDOWN_TIME;;
-        battleBehaviorComponent.attackType = BattleBehaviorComponent.AttackType.SCISSORS;
+		BattleBehaviorComponent battleBehaviorComponent = ecs.getMapper(BattleBehaviorComponent.class).get(e);
+		battleBehaviorComponent.swingTime = SWING_TIME;
+		battleBehaviorComponent.cooldownTime = COOLDOWN_TIME;;
+		battleBehaviorComponent.attackType = BattleBehaviorComponent.AttackType.SCISSORS;
 
-        BattleAttackableComponent battleAttackable = ecs.getMapper(BattleAttackableComponent.class).get(e);
-        battleAttackable.hp = HP;
-        battleAttackable.armorType = BattleAttackableComponent.ArmorType.SCISSORS;
+		BattleAttackableComponent battleAttackable = ecs.getMapper(BattleAttackableComponent.class).get(e);
+		battleAttackable.hp = HP;
+		battleAttackable.armorType = BattleAttackableComponent.ArmorType.SCISSORS;
 
-        return e;
-    }
+		return e;
+	}
 
 //    public static void createPhysics(World ecs, Entity e, com.badlogic.gdx.physics.box2d.World box2dWorld, float x, float y) {
 //        Common.createCircleBodyForPhysicsBody(ecs, e, box2dWorld, 1, x, y);
